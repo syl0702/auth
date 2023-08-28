829314ac9a2dd8cfe219db8cf7f11d5133676987
syl0702 <navyblue794@gmail.com>
2023-08-28 15:13:23 +0900

    logout 기능구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 def signup(request):
@@ -35,3 +36,7 @@ def login(request):
     }
 
     return render(request, 'login.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:login')
