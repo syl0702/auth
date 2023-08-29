8194f92063ca49a97cf8858e5ac7af629813de81
syl0702 <navyblue794@gmail.com>
2023-08-29 10:07:34 +0900

    article, comment 모델링

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+    # article_set = 특정 유저가 쓴 글을 볼 수 있는 세트
+    # comment_set = 
--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,28 @@
 from django.db import models
+from accounts.models import User
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
 class Article(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    # comment_set = 장고가 자동으로 추가해주는 칼럼
 
-    # user = models.ForeignKey()
+    # 유저 모델을 참조하는 경우
+    # 방법1. (권장하지 않음)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # 방법2. (권장) settings.AUTH_USER_MODEL == 'accounts.User'
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # 방법3.
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    # user_id = 숫자가 저장됨
+
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    # article_id = 장고가 자동으로 추가해주는 칼럼
+
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    # user_id =
