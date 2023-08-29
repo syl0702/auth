9b628071c4da6e9c175faf8c5ec32f777d0a5531
syl0702 <navyblue794@gmail.com>
2023-08-29 11:48:22 +0900

    updat accounts.views

--- accounts/views.py
+++ accounts/views.py
@@ -5,11 +5,15 @@ from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('accounts:signup')
+            user = form.save()
+            auth_login(request, user)
+            return redirect('articles:index')
         
     else:
         form = CustomUserCreationForm()
@@ -22,6 +26,9 @@ def signup(request):
 
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
     if request.method=='POST':
         form = CustomAuthenticationForm(request, request.POST)
         if form.is_valid():
