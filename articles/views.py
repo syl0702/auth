7780bb100c4f2d75772c7cc516d50131852c2119
syl0702 <navyblue794@gmail.com>
2023-08-29 15:29:08 +0900

    Comment Read 기능 구현

--- articles/views.py
+++ articles/views.py
@@ -39,6 +39,7 @@ def create(request):
     return render(request, 'form.html', context)
 
 
+@login_required
 def comment_create(request, article_id):
     article = Article.objects.get(id=article_id)
 
